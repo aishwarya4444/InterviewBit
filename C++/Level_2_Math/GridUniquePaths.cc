a21ed989a648243e0036fa3b4ce92443858fa47b
Aishwarya <aishwarya4444@gmail.com>
2019-01-03 13:15:26 +0530

    Level 2 : Grid Unique Paths
    Moving codes up by a level

---
+++ C++/Level_2_Math/GridUniquePaths.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+  public:
+    int uniquePaths(int, int);
+};
+
+int Solution::uniquePaths(int A, int B) {
+  int dp[A][B];
+  int r, c;
+  for(r=0;r<A;r++) {
+    dp[r][0] = 1;
+  }
+  for(c=0;c<B;c++) {
+    dp[0][c] = 1;
+  }
+  for(r=1;r<A;r++) {
+    for(c=1;c<B;c++) {
+      dp[r][c] = dp[r][c-1] + dp[r-1][c];
+    }
+  }
+  return dp[A-1][B-1];
+}
+
+
+int main(void ) {
+  Solution s;
+  cout<<s.uniquePaths(2,2)<<endl;
+
+  return 0;
+}
